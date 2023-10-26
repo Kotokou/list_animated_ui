3c75be70203fc57cef64c65b0b973a9f51195c4a
KOTOKOU Komla David <kotokou74@gmail.com>
2023-10-26 18:49:17 +0000

    completed shrink page

--- lib/screens/shrink_top_list_screen.dart
+++ lib/screens/shrink_top_list_screen.dart
@@ -18,6 +18,7 @@ class _ShrinkTopListScreenState extends State<ShrinkTopListScreen> {
 
   @override
   void initState() {
+    characters.addAll(List.from(characters));
     scrollController.addListener(onListen);
     super.initState();
   }
@@ -31,53 +32,81 @@ class _ShrinkTopListScreenState extends State<ShrinkTopListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Shrink List"),
-      ),
+      backgroundColor: Colors.white,
+      // appBar: AppBar(
+      //   title: const Text("Shrink Top List"),
+      // ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: CustomScrollView(
           controller: scrollController,
           slivers: [
+            const SliverToBoxAdapter(
+              child: Placeholder(
+                fallbackHeight: 100,
+              ),
+            ),
+            const SliverAppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: Colors.transparent,
+              pinned: true,
+              title: Text(
+                'My Characters',
+                style: TextStyle(color: Colors.black),
+              ),
+              centerTitle: true,
+            ),
+            const SliverToBoxAdapter(
+              child: SizedBox(height: 50),
+            ),
             SliverList(
               delegate: SliverChildBuilderDelegate(
                 childCount: characters.length,
                 (context, index) {
                   final character = characters[index];
-                  final itemPositionOffset = itemHeight * index;
+                  final itemPositionOffset = (itemHeight * .6) * index;
                   final difference =
                       scrollController.offset - itemPositionOffset;
-                  final percent = 1 - (difference / itemHeight);
+                  final percent = 1 - (difference / (itemHeight * .6));
                   double opacity = percent;
                   double scale = percent;
                   if (opacity > 1.0) opacity = 1.0;
                   if (opacity < 0.0) opacity = 0.0;
                   if (percent > 1.0) scale = 1.0;
-                  return Opacity(
-                    opacity: opacity,
-                    child: Transform(
-                      alignment: Alignment.center,
-                      transform: Matrix4.identity()..scale(scale, 1.0),
-                      child: Card(
-                        color: Color(character.color),
-                        child: SizedBox(
-                          height: itemHeight,
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(15),
-                                  child: Text(
-                                    character.title,
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 25,
+                  return Align(
+                    heightFactor: .6,
+                    child: Opacity(
+                      opacity: opacity,
+                      child: Transform(
+                        alignment: Alignment.center,
+                        transform: Matrix4.identity()..scale(scale, 1.0),
+                        child: Card(
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              topRight: Radius.circular(20),
+                            ),
+                          ),
+                          color: Color(character.color),
+                          child: SizedBox(
+                            height: itemHeight,
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(15),
+                                    child: Text(
+                                      character.title,
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 25,
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                              Image.asset(character.avatar),
-                            ],
+                                Image.asset(character.avatar),
+                              ],
+                            ),
                           ),
                         ),
                       ),
